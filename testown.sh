cac5cb9316cce258d3f79f0ccc40d3afdd8cf834
Robbe Decapmaker <debber@dcpm.be>
2023-12-22 14:02:00 +0100

    Make own test a bit harder for the program and easier to interpret

--- testown.sh
+++ testown.sh
@@ -1,6 +1,8 @@
 make all
+rm gateway.log
+rm data.csv
 port=5678
-clients=28
+clients=40
 echo -e "starting gateway "
 valgrind --leak-check=summary ./sensor_gateway $port $clients &
 sleep 3
@@ -67,7 +69,37 @@ sleep 1
 
 ./sensor_node 1 3 127.0.0.1 $port &
 
+./sensor_node 15 1 127.0.0.1 $port &
+
+./sensor_node 21 3 127.0.0.1 $port &
+
+./sensor_node 37 2 127.0.0.1 $port &
+
+./sensor_node 132 3 127.0.0.1 $port &
+
+./sensor_node 142 3 127.0.0.1 $port &
+
+./sensor_node 142 3 127.0.0.1 $port &
+
+./sensor_node 1 3 127.0.0.1 $port &
+
+./sensor_node 15 1 127.0.0.1 $port &
+
+./sensor_node 21 3 127.0.0.1 $port &
+
+./sensor_node 37 2 127.0.0.1 $port &
+
+./sensor_node 132 3 127.0.0.1 $port &
+
 sleep 11
 killall sensor_node
+
+echo "Results for regular nodes"
+cat gateway.log | grep parsed | wc -l && cat gateway.log | grep insertion | wc -l && cat data.csv | wc -l
+
+./sensor_node 2 8 127.0.0.1 $port 
+
+cat gateway.log | grep parsed | wc -l && cat gateway.log | grep insertion | wc -l && cat gateway.log | grep timed | wc -l && cat gateway.log | grep invalid | wc -l && cat data.csv | wc -l 
+
 sleep 3
 killall sensor_gateway
